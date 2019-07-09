80b6fff05a90fcb89965da510a0d74db88194d56
NIKKOR <korobckin.nickita@icloud.com>
2019-07-09 16:26:14 +0300

    Update hw05_easy_DONE.py

--- lesson05/home_work/hw05_easy_DONE.py
+++ lesson05/home_work/hw05_easy_DONE.py
@@ -27,14 +27,13 @@ rmdir_name_x_to_y('dir_', 1, 9)
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-import task1
 
 def item_path(item):
     return os.path.join(os.getcwd(), item)
 
 
 try:
-    task1.mkdir_name_x_to_y('dir_', 1, 9)
+    mkdir_name_x_to_y('dir_', 1, 9)
 
     dir_path_2 = os.listdir(os.getcwd())
     print('\n{0:.^50}'.format('Папки текущей директории:') +
@@ -44,11 +43,10 @@ try:
         if os.path.isdir(item_path(i)):
             print(i, '{:.>100}'.format(extra_path))
 finally:
-    task1.rmdir_name_x_to_y('dir_', 1, 9)
+    rmdir_name_x_to_y('dir_', 1, 9)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-import task2
 
 item_name = str(os.path.basename(sys.argv[0]))[:-3]
-copyfile(task2.item_path(item_name + '.py'), task2.item_path(item_name + '_copy.py'))
+copyfile(item_path(item_name + '.py'), item_path(item_name + '_copy.py'))
